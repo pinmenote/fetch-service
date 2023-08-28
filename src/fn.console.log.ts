4083be5d7ef925ebbe9ddce011307c8fbd987b1e
Michal Szczepanski <michal@vane.pl>
2023-08-28 23:43:23 +0200

    fix: dummy condition escape

--- src/fn.console.log.ts
+++ src/fn.console.log.ts
@@ -1,8 +1,7 @@
 import { Config } from './config';
 
 export const fnConsoleLog = (...args: any[]) => {
-  if (!Config.isProduction) {
-    //eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-argument
-    console.log(...args);
-  }
+  if (Config.isProduction) return;
+  //eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-argument
+  console.log(...args);
 };
